794c2daf10de77df68054fd502fbfbfcd0b3db0a
souljazzfunk <59495284+souljazzfunk@users.noreply.github.com>
2023-09-15 16:13:39 +0900

    updated User-Agent

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -14,9 +14,9 @@ def fetch_data_with_requests(url):
     # Helper function to handle errors
     def exit_with_error(message):
         print(message)
-        return None, None, None
+        exit(1)
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.3'}
     session = requests.Session()
     
     for retry_count in range(5):  # Maximum retries: 5
