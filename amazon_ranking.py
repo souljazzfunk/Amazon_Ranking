3ae6e803a1e252a27619237cdceef48947f7944c
souljazzfunk <59495284+souljazzfunk@users.noreply.github.com>
2023-11-13 23:58:40 +0900

    update screenshot

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -18,11 +18,11 @@ def fetch_data_with_requests(url):
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.3'}
     session = requests.Session()
-    
+
     for retry_count in range(5):  # Maximum retries: 5
         response = session.get(url, headers=headers)
         status_code = response.status_code
-        
+
         if status_code == 200:
             break
         elif status_code == 503:
@@ -31,13 +31,16 @@ def fetch_data_with_requests(url):
         else:
             return exit_with_error(f"Failed to fetch the webpage with an unhandled status code: {status_code}. Exiting.")
     else:
-        return exit_with_error("Maximum retries reached for status code 503. Exiting.")
+        return exit_with_error(f"Maximum retries reached for status code {status_code}. Exiting.")
 
     soup = BeautifulSoup(response.text, 'html.parser')
     ul_elems = soup.select('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list')
     
     if not ul_elems or len(ul_elems) < 2:
-        return exit_with_error("Could not find ranking data.")
+        response.encoding = 'shift_jis'
+        with open('error_log.html', 'w', encoding='shift_jis') as file:
+            file.write(response.text)
+        return exit_with_error(f"Could not find ranking data.\n{ul_elems}")
     
     ul_elem = ul_elems[1]
 
@@ -53,17 +56,21 @@ def fetch_data_with_requests(url):
         category = category.strip().split('(')[0]
         ranking_data[category] = num_rank
 
-        if num_rank <= 15:
+        if num_rank <= 9:
             screenshot_needed = True
             screenshot_links.append(rank.find('a', href=True)['href'])
     
     return screenshot_needed, screenshot_links, ranking_data
 
+def save_screenshot(element, timestamp):
+    img_binary = element.screenshot_as_png
+    img = Image.open(io.BytesIO(img_binary))
+    img.save(f'amazon_ranking_{timestamp.strftime("%Y-%m-%d_%H-%M-%S")}.png')
 
 def take_screenshot_with_selenium(url, screenshot_links):
     chrome_service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    options.add_argument('--window-size=2400,1080')
+    options.add_argument('--window-size=2200,1600')
     options.add_argument('--headless')
     driver = webdriver.Chrome(service=chrome_service, options=options)
 
@@ -71,22 +78,20 @@ def take_screenshot_with_selenium(url, screenshot_links):
     sleep(2)
     div_elm = driver.find_element(By.CSS_SELECTOR, '#detailBulletsWrapper_feature_div')
     driver.execute_script("arguments[0].scrollIntoView();", div_elm)
-    img_binary = div_elm.screenshot_as_png
-    img = Image.open(io.BytesIO(img_binary))
-    img.save(f'amazon_ranking_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
+    save_screenshot(div_elm, datetime.now())
     
     for link in screenshot_links:
         full_link = f'https://www.amazon.co.jp{link}'
         driver.get(full_link)
         sleep(2)
-        
+
+        body = driver.find_element(By.TAG_NAME, 'body')
+        save_screenshot(body, datetime.now())
+    
         div_elm = driver.find_element(By.CSS_SELECTOR, 'div#B0C6D2ZF8D')
         grandparent_elm = div_elm.find_element(By.XPATH, '../../..')
-        
         driver.execute_script("arguments[0].scrollIntoView();", grandparent_elm)
-        img_binary = grandparent_elm.screenshot_as_png
-        img = Image.open(io.BytesIO(img_binary))
-        img.save(f'amazon_ranking_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
+        save_screenshot(grandparent_elm, datetime.now())
     
     driver.quit()
 
